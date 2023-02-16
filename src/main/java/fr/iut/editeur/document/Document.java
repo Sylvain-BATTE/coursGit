2c83f77951b8128aadfa98cff8e2f6be67e892c0
Sylvain_BATTE <adresseMail>
2023-02-16 11:46:12 +0100

    ajoutClear

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -39,4 +39,8 @@ public class Document {
     public void effacer(int start, int end) {
         remplacer(start, end, "");
     }
+
+    public void clear() {
+        this.texte = "Aie ! C'est le bug";
+    }
 }
