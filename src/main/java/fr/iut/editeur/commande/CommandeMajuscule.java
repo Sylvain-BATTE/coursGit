ad50c1c69bd5fd35bdabedaffb5d5d173a8e7c6f
Sylvain_BATTE <adresseMail>
2023-02-16 11:04:39 +0100

    commande majuscule fini

--- src/main/java/fr/iut/editeur/commande/CommandeMajuscule.java
+++ src/main/java/fr/iut/editeur/commande/CommandeMajuscule.java
@@ -2,22 +2,21 @@ package fr.iut.editeur.commande;
 
 import fr.iut.editeur.document.Document;
 
-public class CommandeRemplacer extends CommandeDocument {
+public class CommandeMajuscule extends CommandeDocument {
 
-    public CommandeRemplacer(Document document, String[] parameters) {
+    public CommandeMajuscule(Document document, String[] parameters) {
         super(document, parameters);
     }
 
     @Override
     public void executer() {
-        if(parameters.length < 4) {
-            System.err.println("Format attendu : remplacer;debut;fin;texte");
+        if(parameters.length < 3) {
+            System.err.println("Format attendu : majuscule;debut;fin");
             return;
         }
         int debut = Integer.parseInt(parameters[1]);
         int fin = Integer.parseInt(parameters[2]);
-        String texte = parameters[3];
-        this.document.remplacer(debut, fin, texte);
+        this.document.majuscule(debut, fin);
         super.executer();
     }
 
